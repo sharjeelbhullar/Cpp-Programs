4729ee4f54b5f0ba7100c212a62a29dd2fbefa6c
Sharjeel Ahmad <sharjeelbhullar@gmail.com>
2024-10-24 22:12:22 +0500

    Cpp Datatypes

---
+++ datatypes.cpp
@@ -0,0 +1,10 @@
+//int, long, long long, float, double
+//string and getline
+//char
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+string s = "Hey striver!";
+cout << s;
+return 0;
+}
